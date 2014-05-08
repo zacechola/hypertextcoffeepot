bb2449035a713ab1ce63b202b2c727617e9e49a8
Zac Echola <zac.echola@so.mnscu.edu>
2014-05-08 12:09:25 -0500

    make the dumb thing work.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -52,7 +52,7 @@ function lol() {
     var bar = 3;
     return function (y) {
       console.log( x + y + (++bar));
-    }
+    };
   }
 
   var baz = foo(2);
@@ -60,30 +60,84 @@ function lol() {
   console.log('i can do closures, too.');
 }
 
-
-// Load the bootstrap tooltip
-$('.liar').tooltip();
-
-//load some hipster text
-$(document).ready(function() {
-  $.getJSON('http://hipsterjesus.com/api/?paras=1&type=hipster-centric&html=false', function(data) {
-    $('#hipster').html( data.text );
-  });
-});
+// Alrighty, let's protect some namespace now
+(function($, document, window) {
 
 
-// get some dribble images, links and stuff
-$(document).ready(function() {
-  $.getJSON('http://api.dribbble.com/shots/everyone', function(data) {
-    console.log(data);
-  });
-});
+    // get some dribble images, links and stuff
 
+    $(document).ready(function() {
+    $.ajax({
+        dataType: 'jsonp',
+        url: 'http://api.dribbble.com/shots/everyone?per_page=30',
+        data: 'jsonp',
+        success: function (data) {
 
-$('.media').click(function() {
-    $(this).find('img').addClass('animate moveImg auto-cursor');
-    $(this).addClass('animate shrinkLeft auto-cursor');
-    $(this).bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
-        console.log('animation ended!');
+            var arr = [];
+
+            for(var i = 0, l = data.shots.length; i < l; i++) {
+
+                // Make sure we've got images and titles, because these are important things on secondary 'pages'
+                if (data.shots[i].width > 400 && data.shots[i].description) {
+                    arr.push(data.shots[i]);
+                }
+            }
+
+            // Get a random object from arr
+            var randItem = arr[Math.floor(Math.random()*arr.length)];
+
+            // Cool, MANIPULATE THAT DOM
+            manipulateDatDOM(randItem);
+
+        }
     });
-});
+    });
+
+
+
+
+    // Manipulate that DOM
+    function manipulateDatDOM(data) {
+
+        console.log(data);
+
+        var imageUrl = data.image_url,
+            image_sm = data.image_400_url,
+            url = data.url,
+            title = data.title,
+            desc = data.description,
+            playerName = data.player.username,
+            playerUrl = data.player.url,
+            template = '<img class="media-object" src="' + imageUrl + '" alt="' + title + '" >';
+
+        // Create the image
+        $('.media').append(template);
+
+        // Drop in event listener for clicky of imagey
+        $('.media').click(function() {
+            console.log(this._currentBrowser);
+            $(this).find('img').addClass('animate moveImg auto-cursor');
+            $(this).addClass('animate shrinkLeft auto-cursor');
+            $('img').bind("animationend webkitAnimationEnd MSAnimationEnd", function(){
+                // When the animation has ended, load in some info
+                var template = '<h2><a href="' + url + '">' + title + '</a></h2><p>' + desc + '</p><p>Artwork by: <a href="' + playerUrl + '">' + playerName + '</a></p>';
+                $('.content').append(template);
+            });
+        });
+
+    }
+
+
+
+
+    // Load the bootstrap tooltip
+    $('.liar').tooltip();
+
+    //load some hipster text
+    $(document).ready(function() {
+        $.getJSON('http://hipsterjesus.com/api/?paras=1&type=hipster-centric&html=false', function(data) {
+            $('#hipster').html( data.text );
+        });
+    });
+
+})(jQuery, document, window);
